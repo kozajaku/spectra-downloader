4c0cc3b38f45739790960b602cb8860cd563c4c8
Jakub Koza <kozajaku@fit.cvut.cz>
2017-01-29 21:17:03 +0100

    Migration of downloader to new directory
    This is just preparation before migration the python module into new
    repository.

---
+++ spectra_downloader/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='spectra-downloader',
+    version='0.1',
+    description='Simple Python API for parsing SSAP query VOTABLEs and download spectra using '
+                'direct method or DataLink protocol.',
+    long_description=long_description,
+    author='Jakub Koza',
+    author_email='kozajaku@fit.cvut.cz',
+    keywords='astronomy,SSAP,DataLink,spectra,spectrum',
+    license='MIT',
+    url='https://github.com/kozajaku/spectra-analyzer',# todo change after migration
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    install_requires=['requests'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'flexmock', 'betamax'],
+)
